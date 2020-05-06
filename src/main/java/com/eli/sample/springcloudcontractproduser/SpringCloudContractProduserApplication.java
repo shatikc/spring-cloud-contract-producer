63eb3836cde87241edbadf21c06c650a5fbc0614
EliG <shatikc@gmail.com>
2020-05-06 11:20:45 +0300

    init commit

---
+++ src/main/java/com/eli/sample/springcloudcontractproduser/SpringCloudContractProduserApplication.java
@@ -0,0 +1,13 @@
+package com.eli.sample.springcloudcontractproduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContractProduserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudContractProduserApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/eli/sample/springcloudcontractproduser/controller/EvenOddController.java
@@ -0,0 +1,15 @@
+package com.eli.sample.springcloudcontractproduser.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EvenOddController {
+
+    @GetMapping("/validate/prime-number")
+    public String isNumberPrime(@RequestParam(name = "number") int number) {
+        return number % 2 == 0 ? "Even" : "Odd";
+    }
+
+}
